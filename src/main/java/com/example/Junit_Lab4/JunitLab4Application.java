daa0539ddb0bfe703b8aa1b904b03a9a1294ae9b
emmadahl <emmadahl80@gmail.com>
2023-04-18 14:02:57 +0200

    All files

---
+++ src/main/java/com/example/Junit_Lab4/JunitLab4Application.java
@@ -0,0 +1,13 @@
+package com.example.Junit_Lab4;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class JunitLab4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitLab4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Junit_Lab4/JunitLab4ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.Junit_Lab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class JunitLab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
